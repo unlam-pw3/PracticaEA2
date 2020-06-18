644977d074a2ec5602e634b8136b2856beea363f
psanchez <psanchez@plus54.com>
2020-06-17 22:03:44 -0300

    agregando servicio webapi con get de todos los Animales

---
+++ PracticaEA2/Controllers/AnimalesApiController.cs
@@ -0,0 +1,37 @@
+﻿using DAL;
+using PracticaEA2.DTOs;
+using Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PracticaEA2.Controllers
+{
+    public class AnimalesApiController : ApiController
+    {
+        IAnimalesServicio AnimalesServicio;
+
+        public AnimalesApiController()
+        {
+            PracticaWebServicesEntities ctx = new PracticaWebServicesEntities();
+            AnimalesServicio = new AnimalesServicio(ctx);
+        }
+
+        // GET api/<controller>
+        public List<AnimalDTO> Get()
+        {
+            List<Animal> animalesEF = AnimalesServicio.ObtenerTodos();
+            List<AnimalDTO> animalesDTO = new List<AnimalDTO>();
+            foreach (var animalEF in animalesEF)
+            {
+                AnimalDTO dto = new AnimalDTO(animalEF);
+                animalesDTO.Add(dto);
+            }
+            return animalesDTO;
+        }
+       
+    }
+}
---
+++ PracticaEA2/DTOs/AnimalDTO.cs
@@ -0,0 +1,33 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace PracticaEA2.DTOs
+{
+    [DataContract]
+    public class AnimalDTO
+    {
+        [DataMember]
+        public int IdAnimal { get; set; }
+        [DataMember]
+        public string NombreEspecie { get; set; }
+        [DataMember]
+        public double PesoPromedio { get; set; }
+        [DataMember]
+        public int EdadPromedio { get; set; }
+        [DataMember]
+        public int IdTipoAnimal { get; set; }
+
+        public AnimalDTO(Animal animal)
+        {
+            this.IdAnimal = animal.IdAnimal;
+            this.NombreEspecie = animal.NombreEspecie;
+            this.PesoPromedio = animal.PesoPromedio;
+            this.EdadPromedio = animal.EdadPromedio;
+            this.IdTipoAnimal = animal.IdTipoAnimal;
+        }
+    }
+}
